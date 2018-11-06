aee965225767e01c55907ee50b8d50084c867c51
Wolfgang Zolleis <wzolleis@web.de>
2018-11-06 22:32:25 +0100

    Speichert einen User in der Datenbank ab

--- src/auth/passport.ts
+++ src/auth/passport.ts
@@ -1,21 +1,41 @@
 import passport from 'passport'
 import keys from '../config/keys'
 const GoogleStrategy: any = require('passport-google-oauth20').Strategy;
+import mongoose = require("mongoose");
+import { SchemaNames } from '../db/SchemaNames'
+import {  UserDocument } from '../user/UserTypes'
+const uuidv4 = require('uuid/v4')
 
-export const initializePassport: () => void  = () => {
-    passport.use(
-        // 'http://localhost:5000/auth/google/callback'
-        new GoogleStrategy(
-            {
-                clientID: keys.googleClientID,
-                clientSecret: keys.googleClientSecret,
-                callbackURL: '/auth/google/callback'
-            },
-            (accessToken: string, refreshToken: string, profile: any, done: any) => {
-                console.log('accessToken: ', accessToken);
-                console.log('refreshToken: ', refreshToken);
-                console.log('profile:', profile);
-            }
-        )
-    );
+export type GoogleProfile = {
+    id: string
+};
+
+export class Passport {
+    static initializePassport: () => void  = () => {
+        const UserModel = mongoose.model(SchemaNames.users);
+
+        passport.use(
+            // 'http://localhost:5000/auth/google/callback'
+            new GoogleStrategy(
+                {
+                    clientID: keys.googleClientID,
+                    clientSecret: keys.googleClientSecret,
+                    callbackURL: '/auth/google/callback'
+                },
+                (accessToken: string, refreshToken: string, profile: GoogleProfile, done: any) => {
+                    const user: UserDocument = {
+                        googleId: profile.id,
+                        authType: 'google',
+                        userId: uuidv4()
+                    };
+                    console.log('user', user);
+                    new UserModel(user).save();
+                }
+
+            )
+        );
+    }
 }
+
+
+
