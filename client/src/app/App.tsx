eb331cc99a1fb5fbd0400ed63ad75f86e174680b
Wolfgang Zolleis <wzolleis@web.de>
2019-02-24 21:38:23 +0100

    dependency update and Header Component

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const ClanWarLeagueNew = () => <h2>ClanWarLeagueNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <BrowserRouter>
+          <div>
+            <Header/>
+            <Route path={"/"} exact={true} component={Landing}/>
+            <Route path={"/cwls"} exact={true} component={Dashboard}/>
+            <Route path={"/cwls/new"} exact={true} component={ClanWarLeagueNew}/>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export interface HeaderProps {
+}
+
+export class Header extends React.Component<HeaderProps> {
+  public render() {
+    return (
+      <div>
+        bla
+      </div>
+    );
+  }
+}
