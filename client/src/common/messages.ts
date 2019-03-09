4eca4e0b7cbb9bec44528ef640267e303679bc41
Wolfgang Zolleis <wzolleis@web.de>
2019-03-09 21:35:16 +0100

    clan buttons mit style und css-grid

---
+++ client/src/common/messages.ts
@@ -0,0 +1,7 @@
+export enum Messages {
+  delete = 'Löschen',
+  details = 'Details',
+  refresh = 'Aktualisieren',
+  clanWar = 'War',
+  clanLeague = 'Liga'
+}
--- client/src/components/clan/ClanComponent.tsx
+++ client/src/components/clan/ClanComponent.tsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { ClanModel } from '../../app/applicationTypes';
+import '../../css/clanComponent.css';
+import { Messages } from '../../common/messages';
+import { Link } from 'react-router-dom';
+import { AppLinks } from '../../app/AppLinks';
 
 interface ClanComponentProps {
   clan: ClanModel
 }
 
 class ClanComponent extends Component<ClanComponentProps> {
+
   render() {
-    console.log(this.props.clan);
+
     return (
+
       <div>
         <Card>
           <Card.Header>{this.props.clan.name} {this.props.clan.tag}</Card.Header>
@@ -19,7 +24,17 @@ class ClanComponent extends Component<ClanComponentProps> {
             <Card.Text>
               Level: {this.props.clan.clanLevel}
             </Card.Text>
-            <Button variant='primary'>Aktualisieren</Button>
+            <div className={'clan buttons'}>
+              <button className='btn btn-primary clan-button'>{Messages.refresh}</button>
+              <Link
+                className={'btn btn-info clan-button'}
+                to={AppLinks.CLANS_NEW}>
+                {Messages.details}
+              </Link>
+              <button className='btn btn-info clan-button'>{Messages.clanWar}</button>
+              <button className='btn btn-info clan-button'>{Messages.clanLeague}</button>
+              <button className='btn btn-danger clan-button'>{Messages.delete}</button>
+            </div>
           </Card.Body>
         </Card>
       </div>
