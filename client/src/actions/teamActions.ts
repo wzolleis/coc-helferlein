278c1acef435731c0aee2f14243f303226caacaa
Wolfgang Zolleis <wzolleis@web.de>
2019-03-16 22:47:46 +0100

    refactorings

--- client/src/actions/teamActions.ts
+++ client/src/actions/teamActions.ts
@@ -2,9 +2,14 @@ import { TeamActions } from './actionTypes';
 import { actionCreator } from './index';
 import { PlayerModel } from '../app/teamTypes';
 import { createPlayers } from '../data/players';
+import { Action } from 'typescript-fsa';
 
-export const FetchPlayerAction = actionCreator<{ players: PlayerModel[] }>(TeamActions.FETCH_PLAYERS);
+interface FetchPlayerActionPayload {
+  players: PlayerModel[]
+}
 
-export const doFetchPlayers = () => {
+export const FetchPlayerAction = actionCreator<FetchPlayerActionPayload>(TeamActions.FETCH_PLAYERS);
+
+export const doFetchPlayers = (): Action<FetchPlayerActionPayload> => {
   return FetchPlayerAction({ players: createPlayers() });
 };
