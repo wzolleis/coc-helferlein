5e5007908fbf7e98872b25739bbb5f21332e87b1
wzolleis <wzolleis@web.de>
2019-03-17 19:27:00 +0100

    PlayerList als Beispiel fuer Formik

--- client/src/components/teams/PlayerList.tsx
+++ client/src/components/teams/PlayerList.tsx
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
 import { PlayerModel } from '../../app/teamTypes';
-import Player from './Player';
+// import Player from './Player';
+import {  Field, FieldProps, Form, Formik, FormikActions, FormikProps } from 'formik';
 
 interface PlayerListProps {
   players: PlayerModel[]
 }
 
-class PlayerList extends Component<PlayerListProps> {
+interface MyFormValues {
+  firstName: string;
+}
 
-  mapToComponent(player: PlayerModel) {
-    return (<Player key={player.id} player={player}/>);
+class PlayerList extends Component<PlayerListProps> {
+  constructor(props) {
+    super(props);
+    this.renderField = this.renderField.bind(this);
+    this.renderForm = this.renderForm.bind(this);
   }
 
-  render() {
+  handleSubmit(values: MyFormValues, actions: FormikActions<MyFormValues>) {
+    console.log({ values, actions });
+    alert(JSON.stringify(values, null, 2));
+    actions.setSubmitting(false);
+  }
+
+  renderField(fieldProps: FieldProps<MyFormValues>) {
+    const { field, form } = fieldProps;
     return (
       <div>
-        {this.props.players.map(this.mapToComponent)}
+        <input type='text' {...field} placeholder='First Name'/>
+        {form.touched.firstName && form.errors.firstName && form.errors.firstName}
+      </div>
+    );
+  }
+
+  renderForm( _: FormikProps<MyFormValues>) {
+
+
+    return (
+      <Form>
+        <Field
+          name='firstName'
+          render={this.renderField}
+        />
+      </Form>
+    );
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div>
+        <h1>My Example</h1>
+        <Formik
+          initialValues={{ firstName: '' }}
+          onSubmit={this.handleSubmit}
+          render={this.renderForm}
+        />
       </div>
     );
   }
