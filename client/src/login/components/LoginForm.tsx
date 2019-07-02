3959ae2c93a826af2e5e833fece34bf701937cbc
Wolfgang Zolleis <wzolleis@web.de>
2019-07-02 21:35:18 +0200

    LoginForm mit Validation

--- client/src/login/components/LoginForm.tsx
+++ client/src/login/components/LoginForm.tsx
@@ -1,60 +1,72 @@
 import React from 'react';
-import { Field, Formik } from 'formik';
+import { Field, Formik, FormikActions } from 'formik';
+
+interface LoginFormProps {
+}
 
 interface LoginFormData {
   username: string
   password: string
 }
 
-const INITAL_VALUES: LoginFormData = { username: 'user', password: '' };
+const INITAL_VALUES: LoginFormData = { username: '', password: '' };
 
-const submitForm = (values, { setSubmitting }) => {
+const submitForm = (values: LoginFormData, actions: FormikActions<LoginFormData>) => {
+  const { setSubmitting } = actions;
+  setSubmitting(true);
   setTimeout(() => {
     alert(JSON.stringify(values, null, 2));
     setSubmitting(false);
   }, 400);
 };
 
-export const LoginForm = (props: LoginFormData) => {
+const validateForm = (values: LoginFormData) => {
+  let errors = {};
+  if (!values.username) {
+    errors['username'] = 'Bitte Name eingeben';
+  }
+
+  if (!values.password) {
+    errors['password'] = 'Bitte Passwort eingeben';
+  }
+
+  return errors;
+};
+
+export const LoginForm = () => {
   return (
     <Formik
       enableReinitialize={true}
       initialValues={INITAL_VALUES}
+      validate={validateForm}
       onSubmit={submitForm}
     >
       {({ handleSubmit }) => (
         <form onSubmit={handleSubmit}>
-          <Field component={TextInput} label='Name' name='username'/>
-          <Field component={PasswordInput} label='Password' name='password'/>
-          <button type={'submit'}>Login</button>
+          <Field component={InputWithLabel}
+                 placeholder='Bitte Namen eingeben'
+                 type='text' id='field-username'
+                 label='Name' name='username'/>
+          <Field component={InputWithLabel}
+                 placeholder='Bitte Passwort eingeben'
+                 type='password' id='field-password'
+                 label='Password' name='password'/>
+          <button className={'btn btn-primary'} type={'submit'}>Anmelden</button>
         </form>
       )}
     </Formik>
   );
 };
 
-const TextInput = ({
-                     field, // { name, value, onChange, onBlur }
-                     form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-                     ...props
-                   }) => (
-  <div>
+const InputWithLabel = ({
+                          field, // { name, value, onChange, onBlur }
+                          form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+                          ...props
+                        }) => (
+  <div className={'form-group'}>
     <label>{props.label}</label>
-    <input type='text' {...field} {...props} />
+    <input className={'form-control '} {...field} {...props} />
     {touched[field.name] &&
-    errors[field.name] && <div className='error'>{errors[field.name]}</div>}
-  </div>
-);
-
-const PasswordInput = ({
-                         field, // { name, value, onChange, onBlur }
-                         form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-                         ...props
-                       }) => (
-  <div>
-    <label>{props.label}</label>
-    <input type='password' {...field} {...props} />
-    {touched[field.name] &&
-    errors[field.name] && <div className='error'>{errors[field.name]}</div>}
+    errors[field.name] && <span className={'text-danger'}>{errors[field.name]}</span>}
   </div>
 );
