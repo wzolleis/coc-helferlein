c60cf6409037bc005d89202e6ddf5a49faa287f1
Wolfgang Zolleis <wzolleis@web.de>
2019-07-09 21:00:11 +0200

    Redirect von Login auf Teammanagement wenn Login erfolgreich war

--- client/src/login/components/LoginFormContainer.tsx
+++ client/src/login/components/LoginFormContainer.tsx
@@ -3,17 +3,27 @@ import { LoginForm } from './LoginForm';
 import '../../css/app/form.css';
 import { connect } from 'react-redux';
 import { doLoginUser } from '../../app/actions/appActions';
+import { AppState, LoginState } from '../../app/applicationTypes';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { AppLinks } from '../../app/AppLinks';
 
 interface LoginFormContainerDispatch {
   loginUser: (username: string, password: string) => void
 }
 
-interface LoginFormContainerCombinedProps extends LoginFormContainerDispatch {
+interface LoginFormContainerProps {
+  loginState: LoginState
+}
+
+interface LoginFormContainerCombinedProps extends LoginFormContainerProps, LoginFormContainerDispatch, RouteComponentProps<any> {
 
 }
 
 class LoginFormContainer extends Component<LoginFormContainerCombinedProps> {
   render() {
+    if (this.props.loginState === LoginState.LOGGED_IN) {
+      this.props.history.push(AppLinks.TEAM_MANAGEMENT);
+    }
     return (
       <div className={'form-container'}>
         <LoginForm loginUser={this.props.loginUser}/>
@@ -23,8 +33,10 @@ class LoginFormContainer extends Component<LoginFormContainerCombinedProps> {
 }
 
 
-const mapStateToProps = () => {
-  return {};
+const mapStateToProps = ({ auth }: AppState) => {
+  return {
+    loginState: auth.loginState
+  };
 };
 
-export default connect(mapStateToProps, { loginUser: doLoginUser })(LoginFormContainer);
+export default withRouter(connect(mapStateToProps, { loginUser: doLoginUser })(LoginFormContainer));
