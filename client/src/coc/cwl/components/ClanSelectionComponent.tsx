1808206ae0c484f462d45a218017dc54041c1ddf
wzolleis <wzolleis@web.de>
2019-11-03 19:00:38 +0100

    clan form

--- client/src/coc/cwl/components/ClanSelectionComponent.tsx
+++ client/src/coc/cwl/components/ClanSelectionComponent.tsx
@@ -10,32 +10,55 @@ interface FormProps {
     clanTag?: string;
 }
 
-const InnerForm: React.FC<InjectedFormikProps<FormProps, FormValues>> = (
-    props
-) => (
-    <form onSubmit={props.handleSubmit}>
-        <div className={'form-group'}>
-            <input
-                id='clanTag'
-                placeholder='#xyz123'
-                type='text'
-                onChange={props.handleChange}
-                value={props.values.clanTag}
-            />
-            {props.touched.clanTag && props.errors.clanTag &&
-            <div>{props.errors.clanTag}</div>}
-            <button className={'btn btn-primary'}
-                    type='submit'
-                    disabled={props.isSubmitting}
-            >
-                Submit
-            </button>
+type ValidationVisible = string | boolean;
+
+interface FormValidationProps {
+    showValidation?: ValidationVisible;
+    message?: string;
+}
+
+const FormValidation: React.FC<FormValidationProps> = (props) => {
+    return (
+        <div>
+            {props.showValidation && <div className={'alert alert-danger'}>{props.message}</div>}
         </div>
-    </form>
-);
+    );
+};
+
+const InnerForm: React.FC<InjectedFormikProps<FormProps, FormValues>> = (props) => {
+    const showValidationMessage = props.touched.clanTag && props.errors.clanTag;
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div className={'form-group'}>
+                <label htmlFor='clanTag'>Clan Tag</label>
+                <div className='input-group mb-3'>
+                    <div className='input-group-prepend'>
+                        <span className='input-group-text' id='clan-tag-prepend'>#</span>
+                    </div>
+                    <input
+                        id='clanTag'
+                        placeholder='xyz123'
+                        type='text'
+                        className={'form-control'}
+                        onChange={props.handleChange}
+                        value={props.values.clanTag}
+                    />
+                    <div/>
+                    <div className='input-group-append'>
+                        <button className={'btn btn-primary'} type='submit' disabled={props.isSubmitting}>
+                            Clan War League Informationen
+                        </button>
+                    </div>
+                </div>
+                <FormValidation showValidation={showValidationMessage} message={props.errors.clanTag}/>
+            </div>
+        </form>
+    );
+};
+
 
 const UserSearchForm = withFormik<FormProps, FormValues>({
-    mapPropsToValues: () => ({clanTag: '#QLGYVPQ9'}),
+    mapPropsToValues: () => ({clanTag: 'QLGYVPQ9'}),
     validationSchema: object().shape({
             clanTag: string()
                 .required('ohne Clan Tag geht hier nix')
