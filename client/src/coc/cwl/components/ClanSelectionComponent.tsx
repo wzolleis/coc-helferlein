ea07f7ef3543bf45beb4afb7a3a70acad3c185d6
wzolleis <wzolleis@web.de>
2019-11-03 18:18:16 +0100

    clan tag form

--- client/src/coc/cwl/components/ClanSelectionComponent.tsx
+++ client/src/coc/cwl/components/ClanSelectionComponent.tsx
@@ -1,39 +1,55 @@
+import { InjectedFormikProps, withFormik } from 'formik';
 import React from 'react';
-import { Field, Form, FormikBag, Formik, FormikProps } from 'formik';
+import { object, string } from 'yup';
 
-interface ClanSelectionComponentFormValues {
+interface FormValues {
     clanTag: string;
 }
 
-type ClanSelectionComponentProps = FormikProps<ClanSelectionComponentFormValues>;
+interface FormProps {
+    clanTag?: string;
+}
 
-const ClanSelectionComponent: React.FC<{}> = () => {
-    const initialValues: ClanSelectionComponentFormValues = {clanTag: ''};
-    return (
-        <div>
-            <Formik
-                initialValues={initialValues}
-                onSubmit={(values: ClanSelectionComponentProps, actions) => {
-                    console.log({values, actions});
-                    alert(JSON.stringify(values, null, 2));
-                    actions.setSubmitting(false);
-                }}
-                render={(formikBag: FormikBag<any, ClanSelectionComponentFormValues>) => (
-                    <Form>
-                        <Field
-                            name='clanTag'
-                            render={({field, form, meta}) => (
-                                <div>
-                                    <input type='text' {...field} placeholder='#xyzabc123'/>
-                                    {meta.touched && meta.error && meta.error}
-                                </div>
-                            )}
-                        />
-                    </Form>
-                )}
+const InnerForm: React.FC<InjectedFormikProps<FormProps, FormValues>> = (
+    props
+) => (
+    <form onSubmit={props.handleSubmit}>
+        <div className={'form-group'}>
+            <input
+                id='clanTag'
+                placeholder='#xyz123'
+                type='text'
+                onChange={props.handleChange}
+                value={props.values.clanTag}
             />
+            {props.touched.clanTag && props.errors.clanTag &&
+            <div>{props.errors.clanTag}</div>}
+            <button className={'btn btn-primary'}
+                    type='submit'
+                    disabled={props.isSubmitting}
+            >
+                Submit
+            </button>
         </div>
-    );
-};
+    </form>
+);
 
-export default ClanSelectionComponent;
+const UserSearchForm = withFormik<FormProps, FormValues>({
+    mapPropsToValues: () => ({clanTag: '#QLGYVPQ9'}),
+    validationSchema: object().shape({
+            clanTag: string()
+                .required('ohne Clan Tag geht hier nix')
+        }
+    ),
+    handleSubmit: (values, {setSubmitting}) => {
+        setTimeout(
+            () => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+            },
+            1000
+        );
+    }
+})(InnerForm);
+
+export default UserSearchForm;
