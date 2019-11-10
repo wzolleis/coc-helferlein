c7096594ae0b1bc98f6d523dc18f6f50e7b1cf0a
wzolleis <wzolleis@web.de>
2019-11-10 21:02:59 +0100

    dispatch fetchCwlInfos

--- client/src/coc/cwl/components/ClanWarLeagueContainer.tsx
+++ client/src/coc/cwl/components/ClanWarLeagueContainer.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ClanSelectionComponent from './ClanSelectionComponent';
 import { fetchCwlInfos } from '../actions/cwlActions';
+import { useDispatch } from 'react-redux';
 
 const ClanWarLeagueContainer: React.FC = () => {
+    const dispatch = useDispatch();
+    const onFetchCwlInfos = (clanTag: string) => dispatch(fetchCwlInfos(clanTag));
+
     return (
         <div>
             <h1>Clan War League</h1>
-            <ClanSelectionComponent onFetchCwlInfo={fetchCwlInfos}/>
+            <ClanSelectionComponent onFetchCwlInfo={onFetchCwlInfos}/>
         </div>
     );
 };
